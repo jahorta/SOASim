a364f3d74954cc184dd8d0b1529563874c2b60a7
jahorta <jahorta00000@gmail.com>
2025-09-23 19:24:30 -0500

    edited seed probe to exit on run until bp error

--- SimCore/Phases/Programs/SeedProbe/SeedProbeScript.h
+++ SimCore/Phases/Programs/SeedProbe/SeedProbeScript.h
@@ -3,8 +3,14 @@
 #include "../../../Runner/Script/KeyRegistry.h"
 #include "../../../Core/Memory/Soa/SoaAddrRegistry.h"
 #include "../../../Runner/Breakpoints/BPRegistry.h"
+#include "../BattleRunner/BattleOutcome.h"
 
 namespace simcore::seedprobe {
+
+    static constexpr keys::KeyId DW_Outcome = keys::core::DW_RUN_OUTCOME_CODE;
+    static constexpr keys::KeyId Battle_Outcome = keys::battle::BATTLE_OUTCOME;
+
+    static const std::string LabelDWErr = "RET_DW_RUN_ERROR";
     
     // Build a small program for "apply 1 frame input, then run-until-bp, then read RNG"
     inline PhaseScript MakeSeedProbeProgram()
@@ -20,11 +26,16 @@ namespace simcore::seedprobe {
 
         // Run until RNG seed set breakpoint
         ps.ops.push_back(OpRunUntilBp());
+        ps.ops.push_back(OpGotoIf(DW_Outcome, PSCmp::NE, 0, LabelDWErr));
 
         // Read RNG and emit
         ps.ops.push_back(OpReadU32(addr::Registry::base(addr::core::RNG_SEED), simcore::keys::seed::RNG_SEED));
 
         ps.ops.push_back(OpEmitResult(simcore::keys::seed::RNG_SEED));
+
+        // ============  Label Dolphin Wrapper Run Error  ===================
+        ps.ops.push_back(OpLabel(LabelDWErr));
+        ps.ops.push_back(OpReturnResult(Battle_Outcome, (uint32_t)simcore::battle::Outcome::DWRunErr));
 
         return ps;
     }
