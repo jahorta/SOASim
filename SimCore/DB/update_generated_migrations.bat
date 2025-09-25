9141793ba62ed71c9ae01710d5fc04fb3a1c79ee
jahorta <jahorta00000@gmail.com>
2025-09-25 04:48:24 -0500

    fixed build errors with embedded sql migration

--- SimCore/DB/update_generated_migrations.bat
+++
@@ -1,8 +0,0 @@
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "$out='$(IntDir)GeneratedMigrations.h';" ^
-  "$srcDir='$(ProjectDir)DB\migration';" ^
-  "$files=Get-ChildItem -Path $srcDir -Filter *.sql | Sort-Object Name;" ^
-  "$head='// generated' + [Environment]::NewLine + '#pragma once' + [Environment]::NewLine + '#include <vector>' + [Environment]::NewLine + '#include <string>' + [Environment]::NewLine + 'namespace simcore{namespace db{ static inline const std::vector<std::pair<std::string,std::string>> kEmbeddedMigrations = {';" ^
-  "Set-Content -Path $out -Value $head -Encoding UTF8;" ^
-  "foreach($f in $files){ $name=$f.Name; $txt=[IO.File]::ReadAllText($f.FullName); Add-Content -Path $out -Value ('  {\"'+$name+'\", R\"SQL('+$txt+')SQL\"},') -Encoding UTF8; }" ^
-  "Add-Content -Path $out -Value ('}; }} // ns') -Encoding UTF8;"
